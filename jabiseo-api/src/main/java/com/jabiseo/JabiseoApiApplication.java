5847274698d8764db1772ed8f9236580e285c4e3
morenow <jjang9877@gmail.com>
2024-06-18 16:28:53 +0900

    Refactor(*): 설정 파일을 분리하기 위해 이름을 변경하고 설정하는 코드 추가
    - 다른 모듈들에 yml 파일이 흩어져 있으면 오류 발생
    - System.setProperty() 메소드를 통해 SpringBoot 의존성이 있는 모듈에서 다른 모듈의 yml 파일에 접근

--- jabiseo-api/src/main/java/com/jabiseo/JabiseoApiApplication.java
+++ jabiseo-api/src/main/java/com/jabiseo/JabiseoApiApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JabiseoApiApplication {
 
     public static void main(String[] args) {
+        System.setProperty("spring.config.name", "application-infrastructure,application-domain");
         SpringApplication.run(JabiseoApiApplication.class, args);
     }
 }
