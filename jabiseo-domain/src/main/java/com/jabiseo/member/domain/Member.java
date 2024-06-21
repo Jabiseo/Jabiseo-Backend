03bd8876de5eb9cf640a604adef8ec9c5dfec087
morenow <jjang9877@gmail.com>
2024-06-21 15:38:39 +0900

    Feat(Member): Member Entity 구현
    - soft delete를 위해 @SQLRestriction, @SQLDelete Annotation 활용
    - 현재 자격증 상태를 나타내기 위한 certificateStatus 연관관계 설정 (외래키 제약조건 해제)
    - 마지막 접속 시간은 추후 @LastAccessedDate 와 같은 어노테이션을 통해 적용해보면 좋겠음
    - 많이들 사용하는 BaseTimeEntity를 만들어 상속하는 방법은, 회원을 제외하면 생성 시간, 수정 시간을 모두 사용하는 Entity가 없어 사용하지 않음
    - @EnableJpaAuditing 설정을 위해 JpaConfig 클래스 생성

---
+++ jabiseo-domain/src/main/java/com/jabiseo/member/domain/Member.java
@@ -0,0 +1,54 @@
+package com.jabiseo.member.domain;
+
+import com.jabiseo.certificate.domain.Certificate;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@SQLRestriction("deleted = false")
+@SQLDelete(sql = "UPDATE member SET deleted = true WHERE member_id = ?")
+public class Member {
+
+    @Id
+    @Column(name = "member_id")
+    private String id;
+
+    private String email;
+
+    private String nickname;
+
+    private String profileImage;
+
+    private String oauthId;
+
+    private String oauthServer;
+
+    private boolean deleted = false;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    // TODO : @LastAccessedDate 만들어서 적용해보기
+    private LocalDateTime lastAccessAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "certificate_status_id", foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
+    private Certificate certificateState;
+
+}
