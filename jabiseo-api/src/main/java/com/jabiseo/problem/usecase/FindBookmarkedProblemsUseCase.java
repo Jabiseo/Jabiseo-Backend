9af6abe3d73dc70e41b6ddb76ec131089c489105
morenow <jjang9877@gmail.com>
2024-06-30 01:22:20 +0900

    Feat(Problem): 북마크 전체 목록 조회 API 구현

--- jabiseo-api/src/main/java/com/jabiseo/problem/usecase/FindBookmarkedProblemsUseCase.java
+++ jabiseo-api/src/main/java/com/jabiseo/problem/usecase/FindBookmarkedProblemsUseCase.java
@@ -1,33 +1,28 @@
 package com.jabiseo.problem.usecase;
 
-import com.jabiseo.certificate.dto.ExamResponse;
-import com.jabiseo.certificate.dto.SubjectResponse;
+import com.jabiseo.member.domain.Member;
+import com.jabiseo.member.domain.MemberRepository;
+import com.jabiseo.problem.domain.Bookmark;
 import com.jabiseo.problem.dto.FindBookmarkedProblemsResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class FindBookmarkedProblemsUseCase {
 
-    public List<FindBookmarkedProblemsResponse> execute() {
-        return List.of(
-                new FindBookmarkedProblemsResponse(
-                        "problemId",
-                        new ExamResponse(
-                                "examId",
-                                "description"
-                        ),
-                        new SubjectResponse(
-                                "subjectId",
-                                3,
-                                "name"
-                        ),
-                        true,
-                        "description"
-                )
-        );
+    private final MemberRepository memberRepository;
+
+    public List<FindBookmarkedProblemsResponse> execute(String memberId) {
+        Member member = memberRepository.getReferenceById(memberId);
+        List<Bookmark> bookmarks = member.getBookmarks();
+        return bookmarks.stream()
+                .map(Bookmark::getProblem)
+                .map(FindBookmarkedProblemsResponse::from)
+                .toList();
     }
 }
