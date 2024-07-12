542d535e78c81ca3c98f6d8223619907ef8ec310
Jo In Hyeok <ss112d86@gmail.com>
2024-07-12 13:55:31 +0900

    Feat(cors): cors 셋팅 추가 (#22)
    - cors 허용 (mvc 셋팅, security setting)

---
+++ jabiseo-api/src/main/java/com/jabiseo/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.jabiseo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000","https://devweb.jabiseo.com")
+                .allowedMethods("POST", "GET", "DELETE", "PATCH", "OPTIONS")
+                .allowCredentials(true);
+    }
+
+}
