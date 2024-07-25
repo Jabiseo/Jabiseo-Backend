0ba0b474c61e991fbf9190038dd10dc2c6ebc610
morenow <jjang9877@gmail.com>
2024-07-25 18:34:46 +0900

    Chore(*): 클래스 설명을 위한 단순 주석 추가

--- jabiseo-domain/src/test/java/com/jabiseo/TestConfiguration.java
+++ jabiseo-domain/src/test/java/com/jabiseo/TestConfiguration.java
@@ -2,6 +2,7 @@ package com.jabiseo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// domain 모듈은 @ComponentScan이 없으므로 @DataJpaTest, @SpringBootTest를 사용하지 못한다. @ComponentScan을 추가하기 위한 클래스이다.
 @SpringBootApplication
 public class TestConfiguration {
 }
