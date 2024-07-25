a65085fe9891e22b377c2aca604d4edb3cd3c395
morenow <jjang9877@gmail.com>
2024-07-26 01:04:00 +0900

    Test(*): 북마크 목록 조회의 추가적인 테스트 코드 작성
    - 아래의 경우를 테스트하는 코드 작성
    1. 멤버가 현재 자격증을 가지고 있지 않을 경우
    2. examId와 subjectId가 올바르지 않을 경우

--- jabiseo-api/src/test/java/com/jabiseo/problem/application/usecase/FindBookmarkedProblemsUseCaseTest.java
+++ jabiseo-api/src/test/java/com/jabiseo/problem/application/usecase/FindBookmarkedProblemsUseCaseTest.java
@@ -3,6 +3,8 @@ package com.jabiseo.problem.application.usecase;
 import com.jabiseo.certificate.domain.Certificate;
 import com.jabiseo.member.domain.Member;
 import com.jabiseo.member.domain.MemberRepository;
+import com.jabiseo.member.exception.MemberBusinessException;
+import com.jabiseo.member.exception.MemberErrorCode;
 import com.jabiseo.problem.domain.Problem;
 import com.jabiseo.problem.domain.ProblemRepository;
 import com.jabiseo.problem.dto.FindBookmarkedProblemsResponse;
@@ -24,6 +26,7 @@ import static fixture.MemberFixture.createMember;
 import static fixture.ProblemFixture.createProblem;
 import static fixture.SubjectFixture.createSubject;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 
 @DisplayName("북마크 목록 조회 테스트")
@@ -70,4 +73,20 @@ class FindBookmarkedProblemsUseCaseTest {
         assertThat(results.get(1)).isEqualTo(FindBookmarkedProblemsResponse.from(problem2));
     }
 
+    @Test
+    @DisplayName("현재 자격증이 없는 회원의 북마크 목록 조회를 시도하면 예외처리한다.")
+    void givenMemberWithNoCurrentCertificate_whenFindingBookmarkedProblems_thenReturnError() {
+        //given
+        String memberId = "1";
+        String examId = "3";
+        String subjectId = "4";
+
+        Member member = createMember(memberId);
+        given(memberRepository.getReferenceById(memberId)).willReturn(member);
+
+        //when & then
+        assertThatThrownBy(() -> sut.execute(memberId, Optional.of(examId), List.of(subjectId), 0))
+                .isInstanceOf(MemberBusinessException.class)
+                .hasFieldOrPropertyWithValue("errorCode", MemberErrorCode.CURRENT_CERTIFICATE_NOT_EXIST);
+    }
 }
