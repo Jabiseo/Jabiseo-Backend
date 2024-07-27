83a1c6975760a78158147a17d098522abd6f48c2
morenow <jjang9877@gmail.com>
2024-07-27 18:12:42 +0900

    Fix(*): getter 관련 오류 수정

--- jabiseo-infrastructure/src/main/java/com/jabiseo/client/NetworkApiErrorCode.java
+++ jabiseo-infrastructure/src/main/java/com/jabiseo/client/NetworkApiErrorCode.java
@@ -3,6 +3,7 @@ package com.jabiseo.client;
 import com.jabiseo.common.exception.ErrorCode;
 import lombok.Getter;
 
+@Getter
 public enum NetworkApiErrorCode implements ErrorCode {
     KAKAO_JWK_API_FAIL("카카오 kauth jwk 연결 실패", "NETWORK_001", ErrorCode.INTERNAL_SERVER_ERROR);
 
@@ -14,20 +15,5 @@ public enum NetworkApiErrorCode implements ErrorCode {
         this.message = message;
         this.errorCode = errorCode;
         this.statusCode = statusCode;
-    }
-
-    @Override
-    public String getMessage() {
-        return "";
-    }
-
-    @Override
-    public String getErrorCode() {
-        return "";
-    }
-
-    @Override
-    public int getStatusCode() {
-        return 0;
     }
 }
