72524af632c0ccfc232f0bf9f1a4b0093e86c40f
inhyeok <ss112d86@gmail.com>
2024-09-07 13:14:49 +0900

    Fix(*): plan조회시 createdAt 추가

--- jabiseo-api/src/main/java/com/jabiseo/plan/application/usecase/FindActivePlanUseCase.java
+++ jabiseo-api/src/main/java/com/jabiseo/plan/application/usecase/FindActivePlanUseCase.java
@@ -39,6 +39,7 @@ public class FindActivePlanUseCase {
                 .planId(String.valueOf(plan.getId()))
                 .certificate(CertificateResponse.from(plan.getCertificate()))
                 .endAt(plan.getEndAt())
+                .createdAt(plan.getCreatedAt().toLocalDate())
                 .weeklyPlanItems(planItems.stream().filter((p -> p.getGoalType().equals(GoalType.WEEKLY))).map(PlanItemResponse::from).toList())
                 .dailyPlanItems(planItems.stream().filter((p -> p.getGoalType().equals(GoalType.DAILY))).map(PlanItemResponse::from).toList())
                 .build();
--- jabiseo-api/src/main/java/com/jabiseo/plan/dto/ActivePlanResponse.java
+++ jabiseo-api/src/main/java/com/jabiseo/plan/dto/ActivePlanResponse.java
@@ -5,6 +5,7 @@ import com.jabiseo.problem.dto.CertificateResponse;
 import lombok.Builder;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Builder
@@ -13,6 +14,7 @@ public record ActivePlanResponse(
         CertificateResponse certificate,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         LocalDate endAt,
+        LocalDate createdAt,
         List<PlanItemResponse> dailyPlanItems,
         List<PlanItemResponse> weeklyPlanItems
 ) {
