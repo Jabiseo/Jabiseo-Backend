e840618337bc392c61a2d5aa9fdf56bfea24fdae
morenow <jjang9877@gmail.com>
2024-10-16 00:25:19 +0900

    Feat(Problem): UseCase가 아닌 Service에서 로직을 담당하도록 책임을 옮김
    - 기존에 문제 개수를 잘못 가져오던 현상을 수정

--- jabiseo-api/src/main/java/com/jabiseo/problem/application/usecase/FindProblemsUseCase.java
+++ jabiseo-api/src/main/java/com/jabiseo/problem/application/usecase/FindProblemsUseCase.java
@@ -1,20 +1,18 @@
 package com.jabiseo.problem.application.usecase;
 
 import com.jabiseo.certificate.domain.Certificate;
-import com.jabiseo.certificate.domain.CertificateRepository;
-import com.jabiseo.certificate.exception.CertificateBusinessException;
-import com.jabiseo.certificate.exception.CertificateErrorCode;
-import com.jabiseo.problem.domain.ProblemRepository;
+import com.jabiseo.certificate.domain.Exam;
+import com.jabiseo.certificate.service.CertificateService;
 import com.jabiseo.problem.dto.CertificateResponse;
 import com.jabiseo.problem.dto.FindProblemsResponse;
 import com.jabiseo.problem.dto.ProblemWithBookmarkDetailQueryDto;
 import com.jabiseo.problem.dto.ProblemsDetailResponse;
+import com.jabiseo.problem.service.ProblemService;
 import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -22,28 +20,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FindProblemsUseCase {
 
-    private static final int MAX_PROBLEM_COUNT = 20;
-
-    private final CertificateRepository certificateRepository;
-    private final ProblemRepository problemRepository;
+    private final CertificateService certificateService;
+    private final ProblemService problemService;
 
     //memberId가 null일 경우 비회원이므로 bookmark 유무가 모두 false로 응답된다.
     //examId가 null일 경우 전체 시험을 대상으로 조회한다.
     public FindProblemsResponse execute(@Nullable Long memberId, Long certificateId,
                                         @Nullable Long examId, List<Long> subjectIds, int count) {
 
-        Certificate certificate = certificateRepository.findById(certificateId)
-                .orElseThrow(() -> new CertificateBusinessException(CertificateErrorCode.CERTIFICATE_NOT_FOUND));
-        certificate.validateExamIdAndSubjectIds(examId, subjectIds);
+        Certificate certificate = certificateService.getById(certificateId);
+        certificateService.validateExamIdAndSubjectIds(certificate, examId, subjectIds);
 
-        List<ProblemWithBookmarkDetailQueryDto> problemWithBookmarkDetailQueryDtos = subjectIds.stream()
-                .distinct()
-                .flatMap(subjectId -> {
-                    List<ProblemWithBookmarkDetailQueryDto> problems = problemRepository.findDetailByExamIdAndSubjectIdWithBookmark(memberId, examId, subjectId, MAX_PROBLEM_COUNT);
-                    Collections.shuffle(problems); // 문제 리스트를 랜덤으로 섞음
-                    return problems.stream().limit(count);
-                })
-                .toList();
+        List<ProblemWithBookmarkDetailQueryDto> problemWithBookmarkDetailQueryDtos = getProblemWithBookmarkDetailQueryDtos(memberId, examId, subjectIds, count, certificate);
 
         List<ProblemsDetailResponse> problemsDetailResponses = problemWithBookmarkDetailQueryDtos.stream()
                 .map(ProblemsDetailResponse::from)
@@ -52,5 +40,14 @@ public class FindProblemsUseCase {
         CertificateResponse certificateResponse = CertificateResponse.from(certificate);
 
         return FindProblemsResponse.of(certificateResponse, problemsDetailResponses);
+    }
+
+    private List<ProblemWithBookmarkDetailQueryDto> getProblemWithBookmarkDetailQueryDtos(Long memberId, Long examId, List<Long> subjectIds, int count, Certificate certificate) {
+        if (examId == null) {
+            List<Long> examIds = certificate.getExams().stream().map(Exam::getId).toList();
+            return problemService.findProblemsBySubjectId(memberId, examIds, subjectIds, count);
+        } else {
+            return problemService.findProblemsByExamIdAndSubjectIds(memberId, examId, subjectIds, count);
+        }
     }
 }
--- jabiseo-domain/src/main/java/com/jabiseo/problem/service/ProblemService.java
+++ jabiseo-domain/src/main/java/com/jabiseo/problem/service/ProblemService.java
@@ -9,12 +9,15 @@ import com.jabiseo.problem.exception.ProblemErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ProblemService {
 
+    private static final int MAX_PROBLEM_COUNT = 20;
     private static final int SIMILAR_PROBLEM_COUNT = 3;
 
     private final ProblemRepository problemRepository;
@@ -33,4 +36,37 @@ public class ProblemService {
     public List<ProblemWithBookmarkDetailQueryDto> findProblemsById(Long memberId, List<Long> problemIds) {
         return problemRepository.findDetailByIdsInWithBookmark(memberId, problemIds);
     }
+
+    public List<ProblemWithBookmarkDetailQueryDto> findProblemsBySubjectId(Long memberId, List<Long> examIds, List<Long> subjectIds, int count) {
+        return subjectIds.stream()
+                .distinct()
+                .flatMap(subjectId -> {
+                    List<ProblemWithBookmarkDetailQueryDto> problems =
+                            findProblemsByExamIdsAndSubjectId(memberId, examIds, subjectId, count);
+                    Collections.shuffle(problems); // 문제 리스트를 랜덤으로 섞음
+                    return problems.stream().limit(count);
+                })
+                .toList();
+    }
+
+    public List<ProblemWithBookmarkDetailQueryDto> findProblemsByExamIdAndSubjectIds(Long memberId, Long examId, List<Long> subjectIds, int count) {
+        return subjectIds.stream()
+                .distinct()
+                .flatMap(subjectId -> {
+                    List<ProblemWithBookmarkDetailQueryDto> problems =
+                            problemRepository.findDetailByExamIdAndSubjectIdWithBookmark(memberId, examId, subjectId, MAX_PROBLEM_COUNT);
+                    Collections.shuffle(problems); // 문제 리스트를 랜덤으로 섞음
+                    return problems.stream().limit(count);
+                })
+                .toList();
+    }
+
+    private List<ProblemWithBookmarkDetailQueryDto> findProblemsByExamIdsAndSubjectId(Long memberId, List<Long> examIds, Long subjectId, int count) {
+        return examIds.stream()
+                .flatMap(examId -> problemRepository.findDetailByExamIdAndSubjectIdWithBookmark(memberId, examId, subjectId, MAX_PROBLEM_COUNT)
+                        .stream()
+                        .limit(count))
+                .collect(Collectors.toList());
+    }
+
 }
