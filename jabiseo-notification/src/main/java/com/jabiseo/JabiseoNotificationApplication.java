d8b0c10c409cbd077c670d9719a5081d8f1b6197
inhyeok <ss112d86@gmail.com>
2024-10-17 14:09:16 +0900

    Feat<Noti>: 알림 서버 모듈 및 FCM Config 셋팅

---
+++ jabiseo-notification/src/main/java/com/jabiseo/JabiseoNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.jabiseo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JabiseoNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JabiseoNotificationApplication.class, args);
+	}
+
+}
---
+++ jabiseo-notification/src/test/java/com/jabiseo/JabiseoNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jabiseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JabiseoNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
