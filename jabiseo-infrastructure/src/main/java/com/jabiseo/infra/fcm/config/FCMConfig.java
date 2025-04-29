3496cb6bf83365fda6ee4260fd23385f391cbdb5
inhyeok <ss112d86@gmail.com>
2025-04-29 14:58:07 +0900

    Fix(cicd): 배포 중시로 인한 임시 cicd 삭제

--- jabiseo-infrastructure/src/main/java/com/jabiseo/infra/fcm/config/FCMConfig.java
+++ jabiseo-infrastructure/src/main/java/com/jabiseo/infra/fcm/config/FCMConfig.java
@@ -1,17 +1,21 @@
-package com.jabiseo.notification.config;
+package com.jabiseo.infra.fcm;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import javax.annotation.PostConstruct;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.Executor;
 
 @Slf4j
 @Configuration
@@ -26,6 +30,7 @@ public class FCMConfig {
             ClassPathResource resource = new ClassPathResource(firebaseKeyPath);
             InputStream serviceAccount = resource.getInputStream();
             FirebaseOptions options = FirebaseOptions.builder()
+                    .setThreadManager(new CustomThreadManger()) // custom setup
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
             FirebaseApp.initializeApp(options);
@@ -34,5 +39,16 @@ public class FCMConfig {
         } catch (IOException e) {
             log.error("FirebaseOptions IOException {}" , e.getMessage());
         }
+    }
+
+
+    @Bean(name = "callBackTaskExecutor")
+    public Executor callBackTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setThreadNamePrefix("Jabiseo-FCM-Call-Back-Thread: ");
+        executor.initialize();
+        return executor;
     }
 }
